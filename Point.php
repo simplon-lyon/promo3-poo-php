913658673e68266d825819cb316ae35f5c0bba6a
simplon <simplon@simplon.co>
2017-06-13 15:29:36 +0200

    classe Point et son test

---
+++ Point.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Point
+ *
+ * @author simplon
+ */
+class Point {
+    private $top;
+    private $left;
+    
+    public function __construct(int $top, int $left) {
+        $this->top = $top;
+        $this->left = $left;
+    }
+    
+    public function afficher():string {
+        $style = 'width:2px;'
+                . 'height:2px;'
+                . 'background-color:black;'
+                . 'position:absolute;'
+                . 'top:' . $this->top . 'px;'
+                . 'left:' . $this->left . 'px';
+        
+        return '<div style="' . $style . '"></div>';
+    }
+    /**
+     * Méthode calculant la distance entre ce Point et 
+     * un autre Point
+     * @param Point $point l'instance de point dont on 
+     * va calculer la distance depuis celui où l'on se trouve
+     */
+    //Ici, on type la variable d'entrée pour signifier
+    //que cette méthode n'acceptera qu'une instance de
+    //la classe Point comme paramètre.
+    public function distanceFrom(Point $point): float {
+        return sqrt(pow($point->top-$this->top, 2) 
+                + pow($point->left-$this->left, 2));
+    }
+       
+
+}
--- tp-forme.php
+++ tp-forme.php
@@ -1,8 +1,18 @@
 <?php
-    
+    //On include nos classes
     include_once './Carre.php';
-    
+    include_once './Point.php';
+    //On crée une instance de Carre
     $carreRouge = new Carre(50, 'red');
-    
+    //On affiche sa superficie et le carré
     echo $carreRouge->afficherSuperficie();
     echo $carreRouge->afficher();
+    //On crée 2 instances de Point différentes
+    $pointA = new Point(10, 100);
+    $pointB = new Point(340, 400);
+    //On affiche les deux points
+    echo $pointA->afficher();
+    echo $pointB->afficher();
+    //On calcule la distance entre les deux points
+    echo $pointA->distanceFrom($pointB);
+    
